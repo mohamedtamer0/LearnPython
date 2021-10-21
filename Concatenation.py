74309f24ff92064d0686bd71235618750408ad0f
mohamedtamer0 <mt4452242@gmail.com>
2021-10-21 04:26:00 +0200

    Concatenation

---
+++ Concatenation.py
@@ -0,0 +1,18 @@
+# Concatenation
+
+msg = "I Love"
+lang = "Python"
+print(msg + " " + lang)
+
+full = msg + " " + lang
+print(full)
+
+a = "First \
+Scond \
+Third"
+
+b = "A \
+B \
+C"
+
+print(a + "\n" + b)
