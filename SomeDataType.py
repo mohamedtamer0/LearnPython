7cea61b8002d14c0c49bda4922595b726f83063f
mohamedtamer0 <mt4452242@gmail.com>
2021-10-21 03:42:36 +0200

    update

---
+++ SomeDataType.py
@@ -0,0 +1,26 @@
+# -----------------------------
+# type()
+#
+#
+#
+#
+# -----------------------------
+
+
+print(type(10))  # Int
+print(type(100))  # Int
+print(type(-50))  # Int
+
+print(type(100.9))  # Float
+print(type(1.56546))  # Float
+print(type(-100.9595))  # Float
+
+print(type("Hello Python"))  # String
+
+print(type([1, 2, 3, 4, 5]))  # List
+
+print(type((1, 2, 3, 4, 5)))  # Tuple
+
+print(type({"One": 1, "Two": 2}))  # Dictionary
+
+print(type(2 == 2)) #Boolean
