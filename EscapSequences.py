f07fe2229d3870ab46a3d897c30b0f4310762d2a
mohamedtamer0 <mt4452242@gmail.com>
2021-10-21 04:20:42 +0200

    EscapSequences

---
+++ EscapSequences.py
@@ -0,0 +1,34 @@
+# Back Space
+print("Hello\bWorld")  # Will Remove o
+
+# Escap New Line + \
+print("Hello \
+I Love \
+Python")
+
+
+# Escap Back Slash
+print("I Love Python \\")
+
+# Escap Single Quote
+print('I Love \'Python\'')
+
+# Escap Double Quote
+print("I Love \"Python\"")
+
+
+# Line Feed Or New Line
+print("Hello My name Is Mohamed\nMy Age 25")
+
+
+# Carriage Return
+print("123456\rAbcde")
+
+
+
+
+
+
+
+
+
