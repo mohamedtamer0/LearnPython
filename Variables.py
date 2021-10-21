cc182bca7d7bffb250eafd60d7d77013e079cc64
mohamedtamer0 <mt4452242@gmail.com>
2021-10-21 04:01:51 +0200

    Variables

---
+++ Variables.py
@@ -0,0 +1,29 @@
+# -----------------------------
+# --Variables--
+# Syntax => [Var Name] [Assignment Opr] [Value]
+#
+# -----------------------------
+
+myVar1 = "MyVar"
+_myVar2 = "MyVar2"
+print(myVar1)
+print(_myVar2)
+
+#-----------------------------------#
+
+x = 10
+x = "Hello"
+print(x) #Hello
+
+a,b,c = 1,2,3
+print(a,b,c) # 1 2 3
+
+
+
+
+
+
+
+
+
+
