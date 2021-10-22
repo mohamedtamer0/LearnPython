e7afd4a1f7f74622a00776103692014ce061a082
mohamedtamer0 <mt4452242@gmail.com>
2021-10-23 01:36:44 +0200

    update

--- IF_Elif_Else.py
+++ IF_Elif_Else.py
@@ -4,9 +4,10 @@ cPrice = 100
 cDiscount = 5
 uCountry = "KSA"
 
-if uCountry == "Egypt":
+if uCountry == "Egypt" or uCountry == "Kuwait":
     print(f"Hello {uName} Because You Are From {uCountry}")
     print(f"The Course \"{cName}\" Price Is : ${cPrice - 50}")
+
 
 elif uCountry == "KSA":
     print(f"Hello {uName} Because You Are From {uCountry}")
---
+++ Ternary_Conditional_operator.py
@@ -0,0 +1,11 @@
+# Ternary_Conditional_operator
+
+country = "Egypt"
+
+if country == "Egypt" : print(f"The Weater in {country} If 15")
+else : print("country is Not in The list")
+
+
+# Short If
+age = 2
+print("My Age is 25" if age > 10 else "None")
